b2cb59693d59f4d430969d198d7b1833967252b6
Daniel Beltrán Cruz <beltrancruzdaniel@gmail.com>
2022-01-20 14:03:46 -0600

    176. Children Routes and Lazyload - AuthRoutes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/heros/heros.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AddComponent } from './pages/add/add.component';
+import { HeroComponent } from './pages/hero/hero.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ListComponent } from './pages/list/list.component';
+
+
+
+@NgModule({
+  declarations: [
+    AddComponent,
+    HeroComponent,
+    HomeComponent,
+    ListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HerosModule { }
