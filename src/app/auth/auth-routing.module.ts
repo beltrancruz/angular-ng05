be34900cf90e7eb4aa0af4a4a90691b97ccce516
beltrancruz <beltrancruzdaniel@gmail.com>
2022-01-24 06:57:54 -0600

    Children Routes

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,9 +1,25 @@
 import { NgModule } from '@angular/core';
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
   {
-    path: ''
+    path: '',
+    children: [
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'register',
+        component: RegisterComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'login'
+      }
+    ]
   },
 ]
 
@@ -11,7 +27,10 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    
+    RouterModule.forChild(routes)
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class AuthRoutingModule { }
